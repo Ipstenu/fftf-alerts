ad00bd0642c014c0c9d2fd2d8b0a0f566a32455c
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-07-01 14:04:30 -0700

    Version 1.0
    * Add feature to ensure we don't run the code (and slow sites down) forever.
    * Add upgrade routine
    * Add Cat Signal

--- fftf-alerts.php
+++ fftf-alerts.php
@@ -3,7 +3,7 @@
 Plugin Name: FFTF Alerts
 Plugin URI: https://halfelf.org/plugins/fftf-alerts
 Description: Show Fight for the Future alerts on your website
-Version: 1.0.1
+Version: 1.1.0
 Author: Mika Epstein (Ipstenu)
 Author URI: https://halfelf.org
 License: GPLv3
@@ -39,8 +39,10 @@ Text Domain: fftf-alerts
 class FFTF_Alerts {
 
 	protected static $version;
+	protected static $default_settings;
 	protected static $settings;
 	protected static $fights;
+	protected static $expiration;
 
 	/**
 	 * Constructor
@@ -52,30 +54,39 @@ class FFTF_Alerts {
 		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_script' ) );
 
 		// Create Defaults
-		self::$version    = '1.0.0';
-		$default_settings = array (
+		self::$version    = '1.1.0';
+		self::$expiration = '604800'; // 1 week
+		self::$default_settings = array (
 			'version'          => self::$version,
 			'blackoutcongress' => false,
 			'battleforthenet'  => false,
+			'catsignal'        => false,
 		);
-		self::$settings   = get_option( 'fftf_alerts_options', $default_settings );
+		self::$settings   = get_option( 'fftf_alerts_options', self::$default_settings );
 		self::$fights     = array (
-			'blackoutcongress' => array(
+			'blackoutcongress'     => array(
 				'name'  => __( 'Blackout Congress', 'fftf-alerts' ),
-				'date'  => 'Ongoing',
+				'date'  => 'ongoing',
 				'url'   => 'https://www.blackoutcongress.org',
 				'js'    => 'https://www.blackoutcongress.org/detect.js',
 				'debug' => 'fftf_redirectjs = { alwaysRedirect: true }',
 			),
-			'battleforthenet'  => array(
+			'catsignal' => array(
+				'name'  => __( 'Cat Signal (Internet Defense League)', 'fftf-alerts' ),
+				'date'  => 'ongoing',
+				'url'   => 'https://www.internetdefenseleague.org',
+				'js'    => plugins_url( 'js/catsignal.js', __FILE__ ),
+			),
+			'battleforthenet'       => array(
 				'name'  => __( 'Battle for the Net', 'fftf-alerts' ),
 				'date'  => '2017-07-12',
 				'url'   => 'https://www.battleforthenet.com/july12/',
 				'js'    => 'https://widget.battleforthenet.com/widget.js',
-				'extra' => 'var _bftn_options = { iframe_base_path: "https://widget.battleforthenet.com/iframe" }'
-
+				'extra' => 'var _bftn_options = { iframe_base_path: "https://widget.battleforthenet.com/iframe" }',
 			),
 		);
+
+		$this->check_upgrade();
 	}
 
 	/**
@@ -92,22 +103,57 @@ class FFTF_Alerts {
 	}
 
 	/**
+	 * Check Upgrade
+	 *
+	 * If the version we're running is LESS than the version in the plugin
+	 * then we need to upgrade. Check if a setting is missing and add it.
+	 *
+	 * @since 1.1.0
+	 */
+	public function check_upgrade() {
+		if ( self::$settings['version'] < self::$version ) {
+
+			$the_settings = self::$settings;
+
+			foreach ( self::$default_settings as $setting => $value ) {
+				if ( !array_key_exists( $setting , self::$settings ) ) {
+					$the_settings[$setting] = self::$default_settings[$setting];
+				}
+			}
+
+			$the_settings['version'] = self::$version;
+			ksort( $the_settings );
+
+			update_option( 'fftf_alerts_options', $the_settings );
+		}
+	}
+
+	/**
 	 * Enqueue Scripts
+	 *
+	 * If the setting is true and the date is ongoing, before 7 days AFTER
+	 * the event passes, OR it's in debug mode, enqueue the javascript.
+	 *
 	 * @since 1.0.0
 	 */
 	public function wp_enqueue_script() {
-		// Activate JS based on settings
-		// If the setting is active, then display. Else, don't.
+
 		foreach ( self::$settings as $setting => $value ) {
 			if ( is_bool( $value ) && $value == true ) {
-				wp_enqueue_script( $setting, self::$fights[ $setting ][ 'js' ], array( 'jquery' ), self::$version );
-				if ( array_key_exists( 'extra', self::$fights[ $setting ] ) ) {
-					wp_add_inline_script( $setting, self::$fights[ $setting ][ 'extra' ], 'before' );
-				}
 
-				// Debug mode
-				if ( WP_DEBUG == true && array_key_exists( 'debug', self::$fights[ $setting ] ) ) {
-					wp_add_inline_script( $setting, self::$fights[ $setting ][ 'debug' ], 'before' );
+				$fight = self::$fights[ $setting ];
+				$date  = ( strtotime( $fight['date'] ) == false )? $fight['date'] : strtotime( $fight['date'] );
+
+				if ( $date == 'ongoing' || time() < ( $date + self::$expiration ) || WP_DEBUG == true ) {
+					wp_enqueue_script( $setting, $fight[ 'js' ], array( 'jquery' ), self::$version );
+					if ( array_key_exists( 'extra', $fight ) ) {
+						wp_add_inline_script( $setting, $fight[ 'extra' ], 'before' );
+					}
+
+					// Debug mode
+					if ( WP_DEBUG == true && array_key_exists( 'debug', self::$fights[ $setting ] ) ) {
+						wp_add_inline_script( $setting, self::$fights[ $setting ][ 'debug' ], 'before' );
+					}
 				}
 			}
 		}
@@ -146,6 +192,7 @@ class FFTF_Alerts {
 	function fftf_settings_callback() {
 	    ?>
 	    <p><?php _e( 'To activate an alert for a fight, click the checkbox and save your settings.', 'fftf-alerts' ); ?></p>
+		<p><?php _e( 'In order to prevent you from making your site super slow and load every single script out there, the Cat Signal will deselect everything else for you (and prevent you from being able to select them). It\'s all or piecemeal, basically.', 'fftf-alerts' ); ?></p>
 	    <?php
 	}
 
@@ -158,12 +205,16 @@ class FFTF_Alerts {
 
 		foreach ( self::$settings as $setting => $value ) {
 			if ( is_bool( $value ) ) {
-				$fight   = self::$fights[ $setting ];
+				$fight    = self::$fights[ $setting ];
+				$date     = ( strtotime( $fight['date'] ) == false )? lcfirst( $fight['date'] ) : date_i18n( get_option( 'date_format' ), strtotime( $fight['date'] ) );
 
-				$date = ( strtotime( $fight['date'] ) == false )? $fight['date'] : date_i18n( get_option( 'date_format' ), strtotime( $fight['date'] ) );
+				$disabled = (
+					( strtotime( $date ) == true && time() > ( strtotime( $date ) + self::$expiration ) ) ||
+					( array_key_exists( 'catsignal', self::$settings ) && $setting !== 'catsignal' && self::$settings['catsignal'] == true )
+				)? true : false;
 
 				?>
-				<p><input type="checkbox" id="fftf_alerts_options[<?php echo $setting; ?>]" name="fftf_alerts_options[<?php echo $setting; ?>]" value="1" <?php echo checked( 1, $value ); ?> >
+				<p><input type="checkbox" id="fftf_alerts_options[<?php echo $setting; ?>]" name="fftf_alerts_options[<?php echo $setting; ?>]" value="1" <?php echo checked( 1, $value ); ?> <?php disabled( $disabled, true ); ?> >
 				<label for="fftf_alerts_options[<?php echo $setting; ?>]"><a href="<?php echo $fight['url']; ?>" target="_blank"><?php echo $fight['name']; ?></a> - <?php echo $date; ?></label></p>
 				<?php
 			}
@@ -182,9 +233,25 @@ class FFTF_Alerts {
 
 		foreach ( $options as $setting => $value ) {
 
-			$output[ $setting ] = false;
-			if ( isset( $input[ $setting ] ) && $input[ $setting ] == true ) $output[ $setting ] = true;
+			$fight = self::$fights[ $setting ];
 
+			// Default false
+			$output[ $setting ] = false;
+
+			// If the user set it true, it's true
+			if ( isset( $input[ $setting ] ) && $input[ $setting ] == true ) {
+				$output[ $setting ] = true;
+			}
+
+			// If it's past the sell by date, it's false
+			if ( ( strtotime( $fight[ 'date' ] ) == true && time() > ( $date + self::$expiration ) ) ) {
+				$output[ $setting ] = false;
+			}
+
+			// If the Cat Signal is selected, we ONLY allow that to be checked
+			if ( $input[ 'catsignal' ] == true && $setting !== 'catsignal' ) {
+				$output[ $setting ] = false;
+			}
 		}
 
         $output[ 'version' ] = $options[ 'version' ];
@@ -215,11 +282,10 @@ class FFTF_Alerts {
 
 			<h1><?php _e( 'Fight for the Future Alerts', 'fftf-alerts' ); ?></h1>
 
-			<p><?php echo sprintf( __( '<a href="%1$s" target="_blank">Fight for the Future</a> is dedicated to protecting and expanding the Internet’s transformative power in our lives by creating civic campaigns that are engaging for millions of people.', 'fftf-alerts' ), 'https://fightforthefuture.org' ); ?></p>
-
-			<p><?php _e( 'By default, modals will only display on their designated days. This is by design of Fight for the Future, and not a function of this plugin. If a fight is ongoing, it will always run.', 'fftf-alerts' ); ?></p>
-
 			<?php settings_errors(); ?>
+
+			<p><?php echo sprintf( __( '<a href="%1$s" target="_blank">Fight for the Future</a> is dedicated to protecting and expanding the Internet’s transformative power in our lives by creating civic campaigns that are engaging for millions of people.', 'fftf-alerts' ), 'https://fightforthefuture.org' ); ?></p>
+			<p><?php _e( 'By default, modals will only display on their designated days. This is by design of both Fight for the Future and this plugin. The javascript for a fight will be loaded for seven days after the event, after which it will vanish in order to keep your site snappy. If a fight is ongoing, the javascript will always be loaded.', 'fftf-alerts' ); ?></p>
 
 			<form action="options.php" method="POST" ><?php
 				settings_fields( 'fftf-alerts' );
